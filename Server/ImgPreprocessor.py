285b5a359f460d8341fc24f8d6fa803406152d60
JunHyeon Yoon <junhyun328@gmail.com>
2020-09-24 19:17:45 +0900

    add OCR processor

---
+++ Server/ImgPreprocessor.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class ImgPreprocessor:
+    def __init__(self):
+        self.image = None
+        self.kernel = np.ones((5, 5), np.uint8)
+
+    def ProcExecute(self, file_name):
+        self.image = cv2.imread("./data/" + file_name, cv2.IMREAD_GRAYSCALE)
+        #self.image = cv2.resize(self.image, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+        self.image = cv2.GaussianBlur(self.image, (1, 1), cv2.BORDER_DEFAULT)
+
+        #kernel_sharpen = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        #self.image = cv2.filter2D(self.image, -1, kernel_sharpen)
+
+        #self.image = cv2.adaptiveThreshold(cv2.medianBlur(self.image,3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 41,4)
+        self.image = cv2.adaptiveThreshold(cv2.bilateralFilter(self.image, 3, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 41, 5)
+        cv2.imwrite("./data/tmp.jpg", self.image)
+
+    def display(self):
+        plt.imshow(self.image,cmap="gray"), plt.axis("off")
+        plt.show()
---
+++ Server/OCRProcessor.py
@@ -0,0 +1,38 @@
+import io
+import os
+from ImgPreprocessor import ImgPreprocessor
+from google.cloud import vision
+from google.cloud.vision import types
+
+class OCRProcessor:
+    def __init__(self):
+        self.client = vision.ImageAnnotatorClient()
+
+    def OCRExecute(self, file_name=None):
+
+        if file_name is not None:
+            with io.open("./data/" + file_name, 'rb') as img_file:
+                content = img_file.read()
+
+        drug_img = types.Image(content=content)
+
+        response = self.client.text_detection(image=drug_img)
+        drug_texts = response.text_annotations
+
+        print('Texts: ')
+
+        for text in drug_texts:
+            print('\n"{}"'.format(text.description))
+
+            vertices = (['({},{})'.format(vertex.x, vertex.y)
+                         for vertex in text.bounding_poly.vertices])
+
+            print('bounds: {}'.format(','.join(vertices)))
+
+
+if __name__ == '__main__':
+    img_pproc = ImgPreprocessor()
+    drug_image = img_pproc.ProcExecute("solondo.jpg")
+    img_pproc.display()
+    ocr = OCRProcessor()
+    ocr.OCRExecute("tmp.jpg")
