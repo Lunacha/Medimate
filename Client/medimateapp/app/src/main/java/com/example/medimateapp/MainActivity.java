aa7308d2edb7f6d48bca487df8fe1d38cdf78d9b
JunHyeon Yoon <junhyun328@gmail.com>
2020-10-28 01:48:13 +0900

    add Invoking camera

--- Client/medimateapp/app/src/main/java/com/example/medimateapp/MainActivity.java
+++ Client/medimateapp/app/src/main/java/com/example/medimateapp/MainActivity.java
@@ -1,24 +1,85 @@
 package com.example.medimateapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+    final String TAG = getClass().getSimpleName();
+    EditText identifier1;
+    EditText identifier2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "권한 설정 완료");
+            } else {
+                Log.d(TAG, "권한 설정 요청");
+                requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
     }
 
-    public void onClickSearch(View v)
-    {
-        Intent intent = new Intent(this,DrugListActivity.class);
+    public void onClickSearch(View v) {
+
+
+        Intent intent = new Intent(this, DrugListActivity.class);
         startActivity(intent);
+    }
+
+    public void onClickCamera(View v) {
+
+        openCamera();
+    }
+
+    private void openCamera()
+    {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult");
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode)
+        {
+            case 1:
+                if (resultCode == RESULT_OK && intent.hasExtra("data"))
+                {
+                    Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+
+                }
+                break;
+        }
+
+
     }
 }
